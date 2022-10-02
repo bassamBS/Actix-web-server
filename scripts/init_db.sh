11bdd359bdc13015ca1a654b481db5b503d882e9
bassamBS <bassam-dev@posteo.net>
2022-10-02 17:54:03 +0200

    👷 Add connection to DB

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -18,3 +18,11 @@ docker run \
 -d postgres \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
+
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+>&2 echo "Postgres is still unavailable - sleeping"
+sleep 1
+done
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
